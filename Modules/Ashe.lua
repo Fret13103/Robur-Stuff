468e3c555ca456c460992d30f782abac23d0fdcb
LXMedia1 <anfrbo@gmail.com>
2020-08-16 00:41:00 +0200

    Update Ashe.lua

--- Modules/Ashe.lua
+++ Modules/Ashe.lua
@@ -18,35 +18,18 @@ local function IsReady(spellslot)
     return Player:GetSpellState(spellslot) == SpellStates.Ready
 end
 
-local Q_Blocked_Until = 0
-local function Q_Logic()
-    if IsReady(_Q) then
-        local tick = Game:GetTime() * 1000
-        if Q_Blocked_Until > tick then return end
-        local timeSinceLastAttack = tick - Orb.Data.LastAttack.Time * 1000
-        if (timeSinceLastAttack < 250) then
-            local attackedUnit = Orb.Data.LastAttack.Target
-            if Orb:IsValidAutoAttackTarget(attackedUnit) then
-                Q_Blocked_Until = tick + 250
-                delay(250 - timeSinceLastAttack + Player.AttackCastDelay*1000 + 50, function ()
-                    Input.Cast(_Q)
-                    Orb.LastAttack.Tick = 0
-                end)
-            end
-        end
-    end
-end
 
-local function OnTick()
-    if (Orb.Mode.Combo) then
-        Q_Logic()
+
+local function OnPostAttack()
+    if (IsReady(_Q)) then
+        Input.Cast(_Q)
     end
 end
 
 function OnLoad()
     if Player.CharName == "Ashe" then
         Orb.Load()
-        EventManager.RegisterCallback(Events.OnTick, OnTick)
+        EventManager.RegisterCallback(Events.OnPostAttack, OnPostAttack)
         return true
     end
     return false
